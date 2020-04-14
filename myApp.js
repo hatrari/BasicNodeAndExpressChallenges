c1369b868ca6d7503078c9362dca82a339f70bb2
Atrari Hamza <hatrari@L530342.SQLI>
2020-04-14 18:51:16 +0200

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -48,7 +48,12 @@ app.get('/json-uppercase', (req, res) => {
 
 
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next()
+}, (req, res) => {
+  res.json({time: req.time});
+});
 
 /** 9)  Get input from client - Route parameters */
 
